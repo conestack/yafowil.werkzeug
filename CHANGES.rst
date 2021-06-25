3fafff12556458968168f7ba1ef66b39147183db
Robert Niederreiter <office@squarewave.at>
2021-06-25 13:24:00 +0200

    Package transfer related changes.

---
+++ CHANGES.rst
@@ -0,0 +1,12 @@
+Changelog
+=========
+
+0.2 (unreleased)
+----------------
+
+- No changes yet.
+
+0.1
+---
+
+- Initial release.
--- setup.py
+++ setup.py
@@ -1,41 +1,54 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 import os
 
-version = '0.1'
-shortdesc = 'Werkzeug Integration for YAFOWIL'
-longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
-longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
-tests_require = ['interlude']
 
-setup(name='yafowil.werkzeug',
-      version=version,
-      description=shortdesc,
-      long_description=longdesc,
-      classifiers=[
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Software Development',
-            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-            'License :: OSI Approved :: BSD License',
-      ],
-      keywords='werkzeug request response html input widgets',
-      author='BlueDynamics Alliance',
-      author_email='dev@bluedynamics.com',
-      url=u'http://pypi.python.org/pypi/yafowil.werkzeug',
-      license='Simplified BSD',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['yafowil'],
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          'setuptools',
-          'Werkzeug',
-          'yafowil>=1.3',
-      ],
-      entry_points="""
-      [yafowil.plugin]
-      register = yafowil.werkzeug:register
-      """,
+def read_file(name):
+    with open(os.path.join(os.path.dirname(__file__), name)) as f:
+        return f.read()
+
+
+version = '0.2.dev0'
+shortdesc = 'Werkzeug Integration for YAFOWIL'
+longdesc = '\n\n'.join([read_file(name) for name in [
+    'README.rst',
+    'CHANGES.rst',
+    'LICENSE.rst'
+]])
+
+
+setup(
+    name='yafowil.werkzeug',
+    version=version,
+    description=shortdesc,
+    long_description=longdesc,
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'License :: OSI Approved :: BSD License',
+    ],
+    keywords='werkzeug request response html input widgets',
+    author='Yafowil Contributors',
+    author_email='dev@conestack.org',
+    url=u'http://github.com/conestack/yafowil.werkzeug',
+    license='Simplified BSD',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['yafowil'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        'Werkzeug',
+        'yafowil>=1.3',
+    ],
+    tests_require=[
+        'interlude'
+    ],
+    entry_points="""
+    [yafowil.plugin]
+    register = yafowil.werkzeug:register
+    """,
 )
